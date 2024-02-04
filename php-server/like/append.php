c50cc56633fcb8ac93271f11d004053930d95477
Mark D. Blackwell <markdblackwell01@gmail.com>
2024-02-04 09:57:40 -0500

    PHP server

--- php-server/like/append.php
+++ php-server/like/append.php
@@ -15,34 +15,24 @@ Accept AJAX requests to append song likes to a file.
 function clean_input($string)
 {
     $double_quote = '"';
-    $white_space = "/\s+/";
+    $white_space = '/\s+/';
     $neat = implode(' ', preg_split($white_space, trim($string)));
     return addcslashes($neat, $double_quote);
 };
 
 
 // Constants:
-$double_quote = "\"";
+$double_quote = '"';
 $ip_address = $_SERVER['REMOTE_ADDR'];
-$like_filename = "like.txt";
+$like_filename = 'dynamic/like.txt';
 $my_query_keys = array(
     'song_artist',
     'song_title',
     );
-$n_dash = " - "; // Using hyphen, because it's ASCII.
-$response_ok_json = "";
-
-// d is zero-padded day of the month,
-// H is zero-padded hour of 24,
-// m is zero-padded month,
-// M is zero-padded minute,
-// S is zero-padded second, and
-// Y is year with century.
-$timestamp = date("Y-m-d H:M:S");
+$response_ok_json = '';
+$separator = ' ';
 
 // Depends upon the above:
-
-$prefix = $timestamp . " " . $ip_address;
 
 foreach ($my_query_keys as $key)
     isset($_POST[$key]) or die();
@@ -50,19 +40,27 @@ foreach ($my_query_keys as $key)
 $song_artist = clean_input($_POST['song_artist']);
 $song_title  = clean_input($_POST['song_title' ]);
 
+date_default_timezone_set('UTC');
+
+// c is ISO8601 formatted date and time without fractions of seconds.
+$timestamp = date('c');
+
 $string_to_write =
-    $prefix . $n_dash . $double_quote .
+    $timestamp .
+        $separator .
+    $ip_address .
+        $separator . $double_quote .
     $song_artist .
-    $double_quote . $n_dash . $double_quote .
+        $double_quote . $separator . $double_quote .
     $song_title .
-    $double_quote . "\n";
+        $double_quote . "\n";
 
 // See http://php.net/manual/en/function.fopen.php
 // fopen with "a" means:
 //   "Open for writing only; place the file pointer at the end of the file.
 //   If the file does not exist, attempt to create it."
 
-$myfile = fopen($like_filename, "a") or die();
+$myfile = fopen($like_filename, 'a') or die();
 
 fwrite($myfile, $string_to_write);
 
