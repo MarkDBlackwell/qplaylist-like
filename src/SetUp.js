1bfaa5f6faf6fbc2d0fc92d013aa7c1de350ed8c
Mark D. Blackwell <markdblackwell01@gmail.com>
2024-03-02 11:38:57 -0500

    Files from repository qplaylist-current

---
+++ src/SetUp.js
@@ -0,0 +1,29 @@
+/*
+Copyright (C) 2024 Mark D. Blackwell.
+  All rights reserved.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+'use strict';
+
+(function() {
+	const regexp = /LatestFiveLike(.*)\.html$/;
+	const channel = window.location.pathname.match(regexp)[1];
+	const node = document.querySelector('main');
+
+	const functionConsoleWarningPreventAndLoad = function() {
+//In Firefox, the warning was, "Layout was forced before the page."
+		window.addEventListener('load', function() {
+			Elm.Main.init(
+				{
+					flags: channel,
+					node: node,
+				}
+			);
+		});
+	};
+
+	functionConsoleWarningPreventAndLoad();
+})();
